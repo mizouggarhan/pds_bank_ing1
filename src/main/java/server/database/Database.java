6f701c2965833df41747700d671a5e4718ac03b5
Mouna <Mouna@LAPTOP-8IISSIUE>
2017-06-12 16:16:37 +0200

    résolution bug vm bdd

--- src/main/java/server/database/Database.java
+++ src/main/java/server/database/Database.java
@@ -32,7 +32,7 @@ public class Database {
 
     public static void initPool(){
         try {
-            url = "jdbc:"+composant+"://" + host + ":" + port + "/" + nameDB;
+            url = "jdbc:"+composant+"://" + host + ":" + port + "/" + nameDB+"/useSSL=false";
             Class.forName(driver);
             queue = new ArrayBlockingQueue(LENGTH_QUEUE);
             System.out.println(url);
